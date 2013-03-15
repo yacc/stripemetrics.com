5ecbe4ba46f92e4048fa42f703aae851823e44e0
Yacin Bahi <yacin@sensr.net>
2013-03-14 20:14:34 -0700

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Stripemetrics::Application.config.secret_token = '6166d2d3c9f9b975d4f20820a302d81d417410df62ac79f48e5f506e0bfa1a520e2a92f97d69743112aec8475759e29077c56077c12cedd426c9c871e5a47310'
+Stripemetrics::Application.config.secret_token = '245c774d5bdd877100d3ed195090c36a07b0db15ab880f1f59ce650d4961c1c0029f8de5a3db2b6181b9711fef243c99145145318d4f90e1c1a1334b4f8f7bc0'
