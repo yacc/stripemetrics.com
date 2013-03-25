0b3340e2d7b1995ffb08009232ef5406bfd9f620
Yacin Bahi <yacin@sensr.net>
2013-03-25 15:16:40 -0700

    updating to latest bootstrap 3.0

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Stripemetrics::Application.routes.draw do
   match '/signout'                 => 'sessions#destroy', :as => :signout
   match '/auth/failure'            => 'sessions#failure'
 
-  match 'home/quickstart'          => 'home#quickstart',:as => :quickstart
-  match 'home/faq'                 => 'home#faq',:as => :faq
-  match 'home/pricing'             => 'home#pricing',:as => :pricing
+  match 'quickstart'          => 'home#quickstart',:as => :quickstart
+  match 'faq'                 => 'home#faq',:as => :faq
+  match 'pricing'             => 'home#pricing',:as => :pricing
 
-  match 'account'                  => 'account#index',   :as => :account
-  match 'account/billing'          => 'account#billing',   :as => :billing
+  match 'account'              => 'account#index',   :as => :account
+  match 'billing'              => 'account#billing',   :as => :billing
 
-  match 'dashboard/:action'        => 'dashboard#action',:as => :dashboard
+  match 'dashboard'        => 'dashboard#index',:as => :dashboard
 
   match 'jobs'                     => 'jobs#index',      :as => :jobs      
   match 'jobs/:action'             => 'jobs#action'
