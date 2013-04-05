16917ae4eb16b08e5a207f93d4b5ebe9df1b573b
Yacin Bahi <yacin@sensr.net>
2013-04-05 09:11:04 -0700

    forcing ruby version in Gemfile

---
+++ dotcloud_env.sh
@@ -0,0 +1,12 @@
+dotcloud env set \
+  'GMAIL_USERNAME=yacinb@gmail.com' \
+  'GMAIL_PASSWORD=yacc@google.com' \
+  'ADMIN_NAME=Yacin Bahi' \
+  'ADMIN_EMAIL=yacin@ybsis.com' \
+  'ADMIN_PASSWORD=yacc@stripemetrics' \
+  'ROLES=[admin, user, guest, VIP]'
+
+
+
+
+
