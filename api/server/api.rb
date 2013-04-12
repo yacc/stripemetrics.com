75f78f970d1c6f9f8065901fc9e00e1f2676bc6a
Yacin Bahi <yacin@sensr.net>
2013-04-11 18:44:41 -0700

    Post to auth/tokens to get new token

--- api/server/api.rb
+++ api/server/api.rb
@@ -28,6 +28,12 @@ module Stripemetrics
     # ============================= AUTH =======================================
     namespace :auth do
       desc "Creates and returns access_token if valid credentials"
+      post :tokens do
+        env['warden'].authenticate :password
+        error! "Unauthorized", 401 unless env['warden'].user
+        {:token => env['warden'].user.api_token}
+      end  
+      desc "Returns access_token if valid credentials"
       get :login do
         env['warden'].authenticate :password
         error! "Unauthorized", 401 unless env['warden'].user
