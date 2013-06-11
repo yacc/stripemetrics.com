44d2ba6677b5f748d74b713663e9aba6f1d4bcbe
Yacin Bahi <yacin@sensr.net>
2013-06-11 14:04:28 -0700

    fixes import, simplifies flow, remove impor_director

--- base/spec/models/charges_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe Charge do it "is valid with valid attributes"
-  it "is not valid without a title"
-  it "is not valid without text"
-  it "is not valid without a recipient" 
-end
