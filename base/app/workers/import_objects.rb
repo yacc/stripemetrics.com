adf5e6457cf1283b2cccc0d2bcdd86bb27e47f13
Yacin Bahi <yacin@sensr.net>
2013-06-12 20:29:03 -0700

    fix
    enqueue imports for customer, cde and sde

--- base/app/workers/import_objects.rb
+++ base/app/workers/import_objects.rb
@@ -13,6 +13,12 @@ class ImportObjects
       case type
       when 'ChargeImport'
         user.charge_imports.create(start_at:import.last_imported_ts,end_at:import.end_at,token:import.token,limit:import.limit)        
+      when 'CustomerImport'
+        user.customer_imports.create(start_at:import.last_imported_ts,end_at:import.end_at,token:import.token,limit:import.limit)        
+      when 'CdeImport'
+        user.cde_imports.create(start_at:import.last_imported_ts,end_at:import.end_at,token:import.token,limit:import.limit)        
+      when 'SdeImport'
+        user.sde_imports.create(start_at:import.last_imported_ts,end_at:import.end_at,token:import.token,limit:import.limit)        
       end
     end
   end
