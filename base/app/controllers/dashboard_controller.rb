2a332aef230c96ced2798265a525ec14d5e801fa
Yacin Bahi <yacin@sensr.net>
2013-06-17 19:26:32 -0700

    fix failed charge trend

--- base/app/controllers/dashboard_controller.rb
+++ base/app/controllers/dashboard_controller.rb
@@ -4,8 +4,8 @@ class DashboardController < ApplicationController
 	def index
     @paid_charges     = current_user.paid_charge_volume_trend
     @failed_charges   = current_user.failed_charge_volume_trend
-    @customers          = current_user.acquisition_trend
-    @cancelations       = current_user.cancellation_trend
+    @customers        = current_user.acquisition_trend
+    @cancelations     = current_user.cancellation_trend
   end
 
 end
--- base/app/models/failed_charge_volume_trend.rb
+++ base/app/models/failed_charge_volume_trend.rb
@@ -11,22 +11,19 @@ class FailedChargeVolumeTrend < Trend
 
   def refresh_daily
     self.user.charges.collection.aggregate([match,project,groupby("day")]).collect do |data|
-      vol_usd = data["volume"]/100.0 if data["volume"]
-      [(Time.new(data["_id"]["year"]) + (data["_id"]["day"]).days).to_i*1000, vol_usd ]
+      [(Time.new(data["_id"]["year"]) + (data["_id"]["day"]).days).to_i*1000, data["volume"]/100.0 ]
     end.sort_by{|k|k[0]}
   end
 
   def refresh_weekly
     self.user.charges.collection.aggregate([match,project,groupby("week")]).collect do |data|
-      vol_usd = data["volume"]/100.0 if data["volume"]
-      [(Time.new(data["_id"]["year"]) + (data["_id"]["week"]).weeks).to_i*1000, vol_usd]
+      [(Time.new(data["_id"]["year"]) + (data["_id"]["week"]).weeks).to_i*1000, data["volume"]/100.0]
     end.sort_by{|k|k[0]}
   end
 
   def refresh_monthly
     self.user.charges.collection.aggregate([match,project,groupby("month")]).collect do |data|
-      vol_usd = data["volume"]/100.0 if data["volume"]
-      [(Time.new(data["_id"]["year"]) + (data["_id"]["month"]).month).to_i*1000, vol_usd ]
+      [(Time.new(data["_id"]["year"]) + (data["_id"]["month"]).month).to_i*1000, data["volume"]/100.0 ]
     end.sort_by{|k|k[0]}
   end
 
@@ -54,15 +51,15 @@ class FailedChargeVolumeTrend < Trend
     case interval
     when "day"
       { "$group" =>
-        { "_id" => {"year"=>"$year", "day"=>"$day"}, "count" => { "$sum" => "$amount"}  } 
+        { "_id" => {"year"=>"$year", "day"=>"$day"}, "volume" => { "$sum" => "$amount"}  } 
       }
     when "week"
       { "$group" =>
-        { "_id" => {"year"=>"$year", "week"=>"$week"}, "count" => { "$sum" => "$amount"}  } 
+        { "_id" => {"year"=>"$year", "week"=>"$week"}, "volume" => { "$sum" => "$amount"}  } 
       }
     when "month"      
       { "$group" =>
-        { "_id" => {"year"=>"$year", "month"=>"$month"}, "count" => { "$sum" => "$amount"}  } 
+        { "_id" => {"year"=>"$year", "month"=>"$month"}, "volume" => { "$sum" => "$amount"}  } 
       }
     end
 
