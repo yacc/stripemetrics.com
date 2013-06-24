3e3fe1cc86a9e9e6d17c8d04bf91efd5a11928d2
Yacin Bahi <yacin@sensr.net>
2013-06-24 16:39:13 -0700

    correct last_processed_ts

--- base/app/models/user.rb
+++ base/app/models/user.rb
@@ -83,10 +83,10 @@ class User
 
   def refresh_data
     # get the timestamp of the last imported object
-    last_charge_import = self.charge_imports.last.last_imported_ts    
-    last_customer_import = self.customer_imports.last.last_imported_ts    
-    last_cde_import = self.cde_imports.last.last_imported_ts    
-    last_sde_import = self.sde_imports.last.last_imported_ts
+    last_charge_import = self.charge_imports.order_by(:last_imported_ts.asc).last.last_imported_ts
+    last_customer_import = self.customer_imports.order_by(:last_imported_ts.asc).last.last_imported_ts
+    last_cde_import = self.cde_imports.order_by(:last_imported_ts.asc).last.last_imported_ts
+    last_sde_import = self.sde_imports.order_by(:last_imported_ts.asc).last.last_imported_ts
     # schedule the imports    
     self.charge_imports.create(start_at:Time.now,end_at:last_charge_import,token:self.token,limit:MAX_IMPORTS)        
     self.customer_imports.create(start_at:Time.now,end_at:last_customer_import,token:self.token,limit:MAX_IMPORTS)
