520feacbe522a651d28cd4baaa72ef80343a82ac
Yacin Bahi <yacin@sensr.net>
2013-06-24 14:42:40 -0700

    update warden strategy to support api token in header

--- api/config/initializers/warden.rb
+++ api/config/initializers/warden.rb
@@ -16,11 +16,27 @@ end
 Warden::Strategies.add(:api_token) do
 
   def valid?
-    params && params['api_token']
+    token_value.present?
   end
 
   def authenticate!
     u = User.where(api_token: params['api_token']).first
     u.nil? ? fail!("Could not log in") : success!(u)
   end
+
+  private
+
+  def token_value
+    if header && header =~ /^Token token="(.+)"$/
+      $~[1]
+    else
+      params && params['api_token']
+    end
+  end
+
+  def header
+    request.headers["Authorization"]
+  end
+
 end
+
